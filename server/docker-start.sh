1b724b475d8e26c7bcac5f9b61e8047da3dad481
vernilla <75288193+vernilla@users.noreply.github.com>
2023-08-21 09:09:43 -0400

    Update docker-start.sh

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -5,4 +5,8 @@ cd /opt/app
 alembic upgrade head
 
 echo "Starting szurubooru API on port ${PORT} - Running on ${THREADS} threads"
+<<<<<<< HEAD
 exec waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
+=======
+exec waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
+>>>>>>> parent of 63f687a6 (maybe this will fix it?)
