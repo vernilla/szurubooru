63f687a6cf1e83ffaee496a223208de6c26a3099
Marlowe <admin@chevrine.com>
2023-08-20 23:33:47 -0400

    maybe this will fix it?

--- server/docker-start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/dumb-init /bin/sh
-set -e
-cd /opt/app
-
-alembic upgrade head
-
-echo "Starting szurubooru API on port ${PORT} - Running on ${THREADS} threads"
-exec waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
